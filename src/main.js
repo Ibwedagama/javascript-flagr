5552c8150302bb03a2a3b53cf2d5552a23cdbfb3
Ibwedagama <wardana.wedagama@gmail.com>
2021-06-30 16:03:44 +0700

    setup javascript client flagr

--- src/main.js
+++ src/main.js
@@ -1,6 +1,41 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/**
+ * before import/require `jsflagr` we need to config webpack
+ * check vue.config.file
+ */
+
+var Jsflagr = require('jsflagr');
+
+const flagrClient = new Jsflagr.ApiClient()
+
+/**
+ * override basePath and defaultHeaders
+ */
+flagrClient.basePath = process.env.VUE_APP_FLAGR_BASEPATH
+flagrClient.defaultHeaders = {
+  Authorization: process.env.VUE_APP_FLAGR_AUTH 
+}
+
+const flagrEvaluation = new Jsflagr.EvaluationApi(flagrClient)
+
+// var flagID = 1; // {Number} numeric ID of the flag
+
+var body = Jsflagr.EvalContext.constructFromObject({
+ flagID: 1
+});
+
+var callback = function(error, data, /* response */) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+};
+
+flagrEvaluation.postEvaluation(body, callback)
+
 Vue.config.productionTip = false
 
 new Vue({
